9e450435faf42ae2ecff91559651bdc3e35eeff2
Yazmedov <yazmedov@yandex.ru>
2025-08-22 19:30:50 +0400

    Исправил test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,5 @@
+import allure
+
 from core.Base_Test import browser
 from pages.Base_Page import BasePage
 from pages.Login_Page import LoginPageHelper
@@ -5,12 +7,15 @@ from pages.Login_Page import LoginPageHelper
 BASE_URL = "https://ok.ru/"
 EMPTY_LOGIN_ERROR = "Введите логин"
 
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки при пустой форме авторизации")
 def test_empty_login_end_password(browser):
     BasePage(browser).get_url(BASE_URL)
     Login_Page = LoginPageHelper(browser)
     Login_Page.click_login()
     assert Login_Page.get_error_text() == EMPTY_LOGIN_ERROR
 
+@allure.title("Проверка ошибки ввода пароля при пустом логине")
 def test_password(browser):
     BasePage(browser).get_url(BASE_URL)
     Login_Page = LoginPageHelper(browser)
