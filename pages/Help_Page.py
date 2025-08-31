d803977f97ce0dc3b042eb87e51e09396f83b654
Yazmedov <yazmedov@yandex.ru>
2025-08-31 15:46:54 +0400

    Создал test_help.py

---
+++ pages/Help_Page.py
@@ -0,0 +1,49 @@
+import allure
+from selenium.webdriver import ActionChains
+
+from pages.Base_Page import BasePage
+from selenium.webdriver.common.by import By
+
+class HelpPageLocators:
+    SEARCH_FIELD=(By.XPATH,'//input[@type="search"]')
+    ACTUAL_TODAY = (By.XPATH, '//a[contains(@href, "segodnya-aktualno")]')
+    REGISTRATION = (By.XPATH, '//a[contains(@href, "registraciya")]')
+    MY_PROFILE = (By.XPATH, '//a[contains(@href, "moi-profil")]')
+    COMMUNICATION = (By.XPATH, '//a[contains(@href, "obshchenie")]')
+    PROFILE_ACCESS = (By.XPATH, '//a[contains(@href, "dostup-k-profilu")]')
+    GROUPS = (By.XPATH, '//a[contains(@href, "gruppy")]')
+    PAYED_FUNCTIONS = (By.XPATH, '//a[contains(@href, "platnye-funkcii")]')
+    SPAM = (By.XPATH, '//a[contains(@href, "narusheniya-i-spam")]')
+    GAMES_AND_APPS = (By.XPATH, '//a[contains(@href, "igry-i-prilojeniya")]')
+    OTHER_SERVICES = (By.XPATH, '//a[contains(@href, "drugie-servisy")]')
+    IMPORTANT_INFORMATION = (By.XPATH, '//a[contains(@href, "poleznaya-informaciya")]')
+    ADVERTISEMENT_CABINET = (By.XPATH, '//a[contains(@href, "reklamnyi-kabinet")]')
+
+class HelpPageHelper(BasePage):
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        with allure.step("Проверка корректности загрузки страницы"):
+            self.attach_screenshot()
+        self.find_element(HelpPageLocators.SEARCH_FIELD)
+        self.find_element(HelpPageLocators.ACTUAL_TODAY)
+        self.find_element(HelpPageLocators.REGISTRATION)
+        self.find_element(HelpPageLocators.MY_PROFILE)
+        self.find_element(HelpPageLocators.COMMUNICATION)
+        self.find_element(HelpPageLocators.PROFILE_ACCESS)
+        self.find_element(HelpPageLocators.GROUPS)
+        self.find_element(HelpPageLocators.PAYED_FUNCTIONS)
+        self.find_element(HelpPageLocators.SPAM)
+        self.find_element(HelpPageLocators.GAMES_AND_APPS)
+        self.find_element(HelpPageLocators.OTHER_SERVICES)
+        self.find_element(HelpPageLocators.IMPORTANT_INFORMATION)
+        self.find_element(HelpPageLocators.ADVERTISEMENT_CABINET)
+
+    @allure.step("Скроллим и кликаем на элемент")
+    def scroll_to_item(self, locator):
+        scroll_item = self.find_element(locator)
+        ActionChains(self.driver).scroll_to_element(scroll_item).click(scroll_item).perform()
+
+
